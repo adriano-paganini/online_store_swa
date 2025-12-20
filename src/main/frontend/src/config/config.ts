058f2f0b869eb00d1ed54ba18f1ec4258c7ef180
Illia Bakhmet-Smolenskyi <illia.bakhmet-smolenskyi@student.uibk.ac.at>
2025-12-20 23:57:50 +0100

    ref: ts setup

--- src/main/frontend/src/config/config.ts
+++ src/main/frontend/src/config/config.ts
@@ -4,16 +4,24 @@
  */
 import globalAxios from "axios";
 
+export const BEARER_TOKEN_LOCAL_STORAGE_KEY = "bearerToken";
+
+const API_HOST = window.location.hostname;
+const API_PORT = 8080;
+export const API_BASE_URL = `http://${API_HOST}:${API_PORT}`;
+
+globalAxios.defaults.baseURL = API_BASE_URL;
+
 // Add a request interceptor to add the bearer token to all requests if available
-globalAxios.interceptors.request.use(request => {
+globalAxios.interceptors.request.use(
+  (request) => {
     const accessToken = localStorage.getItem(BEARER_TOKEN_LOCAL_STORAGE_KEY);
     if (accessToken) {
-        request.headers['Authorization'] = `Bearer ${accessToken}`;
+      request.headers["Authorization"] = `Bearer ${accessToken}`;
     }
     return request;
-}, error => {
+  },
+  (error) => {
     return Promise.reject(error);
-});
-
-
-export const BEARER_TOKEN_LOCAL_STORAGE_KEY: string = 'bearerToken';
+  },
+);
