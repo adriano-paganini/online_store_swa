4bb6df70b367e0f50598edc5a61bac07336d35e8
Paganini Adriano <adriano.paganini@student.uibk.ac.at>
2026-01-24 21:09:01 +0100

    feat: order-completion mail done.

--- src/main/java/at/qe/skeleton/repositories/OrderRepository.java
+++ src/main/java/at/qe/skeleton/repositories/OrderRepository.java
@@ -5,6 +5,7 @@ import at.qe.skeleton.model.OrderStatus;
 import at.qe.skeleton.model.Userx;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -18,4 +19,11 @@ public interface OrderRepository extends AbstractRepository<Order, Long> {
             OrderStatus status,
             Pageable pageable
     );
+    //load items, to avoid lazy-loading exception
+    @Query("""
+    select o from Order o
+    left join fetch o.items
+    where o.orderNumber = :orderNumber
+  """)
+    Optional<Order> findByOrderNumberWithItems(String orderNumber);
 }
